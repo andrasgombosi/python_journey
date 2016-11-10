55904702f7ddd4eb99ed16eba81fd90044640a96
Andras <andras.gombosi@gmail.com>
2016-11-10 15:04:41 +0000

    Day 1

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello World"
+print "Hello Again"
+print "tupe"
+print "that"
+print 'Yay! Printing.'
+print "I'd much rather 'not'."
+#print 'I said "do" not touch this.'
---
+++ ex3.py
@@ -0,0 +1,27 @@
+print "Hens", 25+30/6
+print "Roosters", 100.0-25.0*3%4
+
+# evaluates this real time, writes False / True
+print 3+2 < 5-7
+
+#computes, writes end resuls
+print 3+2
+print 5-7
+
+# comma separates text from computations
+print "is it greater?", 5>-2
+print "Is it greater or equal?", 5>=2
+print "Is it less or equal?", 5<=2
+
+
+
+print "Is it less or equal?", 5<=2 , "asd "
+
+print 5/7
+print 5.0/7.0
+
+
+print 3%4
+
+
+print 25 % 9
